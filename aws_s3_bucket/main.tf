7cd001e36454d07058d3c6eceaff9005a531b4fa
Klysman08 <klysman.rk@gmail.com>
2023-07-06 15:47:04 +0100

    bucket s3 aws

---
+++ aws_s3_bucket/main.tf
@@ -0,0 +1,15 @@
+provider "aws" {
+  region = "us-east-1"
+  #profile = "default"
+}
+
+resource "aws_s3_bucket" "terraform-test-bucket" {
+  bucket = "terraform-test-bucket-klysman8728378ffd743"
+
+
+  tags = {
+    Name        = "My bucket Terraform"
+    Environment = "Dev"
+    ManagedBy   = "Terraform"
+  }
+}
